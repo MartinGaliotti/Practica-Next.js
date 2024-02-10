cb3be1ff8e39e426080183b2eaf26ebf8d71b7e2
Martín <martingaliottimartinez@gmail.com>
2024-02-10 18:12:31 -0300

    Paginado

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,21 +2,25 @@
 
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useDebouncedCallback } from 'use-debounce';
+
+const WAIT_BETWEEN_CHANGE = 300;
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const { replace } = useRouter();
   const pathname = usePathname();
 
-  const handleChange = (input: string) => {
+  const handleChange = useDebouncedCallback((input: string) => {
     const params = new URLSearchParams(searchParams);
     if (input) {
       params.set('query', input);
     } else {
       params.delete('query');
     }
+    params.set('currentPage', '1');
     replace(`${pathname}?${params.toString()}`);
-  };
+  }, WAIT_BETWEEN_CHANGE);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -24,7 +28,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         Search
       </label>
       <input
-        value={searchParams.get('query')?.toString()}
+        defaultValue={searchParams.get('query')?.toString()}
         onChange={(event) => handleChange(event.target.value)}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
